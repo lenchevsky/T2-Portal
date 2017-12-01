059810971a45464647885cda5fef8914c18bd4f4
Oleg Snegirev <ol.snegirev@gmail.com>
2017-11-30 22:56:13 -0700

    Switch to MySQL + Travis Integration

--- pos_tools_portal/pos_tools_portal/settings.py
+++ pos_tools_portal/pos_tools_portal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'pos_tools_portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 't2_portal',
+        'USER': 't2_portaluser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
